47157979fa3aa1aea35c2a0ece974e0b1e4f655e
nnastaran83 <nnastaran83@gmail.com>
2022-08-11 16:49:43 +0300

    d

--- main.py
+++ main.py
@@ -1,142 +1,40 @@
-import face_recognition
-import cv2
-import numpy as np
-
-from kivy.logger import Logger
-import logging
-
-Logger.setLevel(logging.TRACE)
-
+# coding:utf-8
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.camera import Camera
-from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
+import cv2
 
 
-def main():
-    FaceRecognition().run()
-    cv2.destroyAllWindows()
-
-class Cam(Widget):
-    pass
-
-
-class FaceRecognition(App):
-
-    def build(self):
-        self.img1 = Image()
-        layout = BoxLayout()
-        layout.add_widget(self.img1)
-        # opencv2 stuffs
-        self.capture = cv2.VideoCapture(0)
-        cv2.namedWindow("CV2 Image")
-        Clock.schedule_interval(self.update, 1.0 / 33.0)
-        return layout
+class KivyCamera(Image):
+    def __init__(self, capture, fps, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = capture
+        Clock.schedule_interval(self.update, 1.0 / fps)
 
     def update(self, dt):
-        # display image from cam in opencv window
         ret, frame = self.capture.read()
-        cv2.imshow("CV2 Image", frame)
-        # convert it to texture
-        buf1 = cv2.flip(frame, 0)
-        buf = buf1.tostring()
-        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-        # if working on RASPBERRY PI, use colorfmt='rgba' here instead, but stick with "bgr" in blit_buffer.
-        texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-        # display image from the texture
-        self.img1.texture = texture1
+        if ret:
+            # convert it to texture
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            # display image from the texture
+            self.texture = image_texture
 
-    def m_build(self):
-        video_capture = Camera(resolution=(640, 480), play=True)
 
-        # TODO: - make this function return true if the person was recognized, else return false.
-        #       - write pydoc
+class CamApp(App):
+    def build(self):
+        self.capture = cv2.VideoCapture(0)
+        self.my_camera = KivyCamera(capture=self.capture, fps=30)
+        return self.my_camera
 
-        # Load a sample picture and learn how to recognize it.
-        obama_image = face_recognition.load_image_file("images/NastaranMotiee.jpg")
-        obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-
-        # Load a second sample picture and learn how to recognize it.
-        biden_image = face_recognition.load_image_file("images/NastaranMotiee.jpg")
-        biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-
-        # Create arrays of known face encodings and their names
-        known_face_encodings = [
-            obama_face_encoding,
-            biden_face_encoding
-        ]
-        known_face_names = [
-            "Nastaran Motiee",
-            "Joe Biden"
-        ]
-
-        # Initialize some variables
-        face_locations = []
-        face_encodings = []
-        face_names = []
-        process_this_frame = True
-        # Grab a single frame of video
-        ret, frame = video_capture.read()
-
-        # Only process every other frame of video to save time
-        if process_this_frame:
-            # Resize frame of video to 1/4 size for faster face recognition processing
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            rgb_small_frame = small_frame[:, :, ::-1]
-
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
-
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
-
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
-
-                face_names.append(name)
-
-        process_this_frame = not process_this_frame
-
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-        return video_capture
+    def on_stop(self):
+        #without this, app will not exit even if the window is closed
+        self.capture.release()
 
 
 if __name__ == '__main__':
-    main()
-
-
-
-jkjhkjjhj
+    CamApp().run()
