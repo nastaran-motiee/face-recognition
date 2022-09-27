91a78c3d66fd362ae2e126acee9eb1f10d221e51
nnastaran83 <nnastaran83@gmail.com>
2022-09-28 00:56:41 +0300

    chanched look

--- app/smart_system.py
+++ app/smart_system.py
@@ -9,11 +9,5 @@ from kivy_camera import KivyCamera
 class SmartSystem(Widget):
     camera = ObjectProperty(None)
 
-   
-   
-
-    
-
-
     def verify_button_clicked(self):
         self.camera.verify_button_action()
