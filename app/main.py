62adae0a804ef59204e7775f7777b3faf68ed777
nnastaran83 <nnastaran83@gmail.com>
2022-09-28 00:54:02 +0300

    design

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 from smart_system import SmartSystem
 from kivymd.app import MDApp
 from kivymd.icon_definitions import md_icons
+from kivymd.uix.screen import MDScreen
 
 
 
@@ -24,7 +25,7 @@ class SmartApp(MDApp):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.material_style = "M3"
   
-        return self.smart_system
+        return MDScreen(self.smart_system)
     
   
     
--- app/smart_system.py
+++ app/smart_system.py
@@ -2,8 +2,18 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy_camera import KivyCamera
 
+
+
+
+
 class SmartSystem(Widget):
     camera = ObjectProperty(None)
+
+   
+   
+
+    
+
 
     def verify_button_clicked(self):
         self.camera.verify_button_action()
